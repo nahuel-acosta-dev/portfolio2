7722158cb6b1c460471b3fe9e517cfe88d159c3f
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-08-16 20:18:35 -0300

    basic structure of section finished house

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -9,12 +9,21 @@ const Home = () => {
             <Container>
                 <Row>
                     <Col>
-                        <div>
+                        <Row>
+                            <Col>
                             <span>Lorem ipsum is placeholder text commonly used</span>
                             <h1 className="title__primary">
                                 Lorem ipsum is placeholder text commonly
                             </h1>
-                        </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                                elit, sed do eiusmod tempor incididunt ut labore et 
+                                dolore magna aliqua. Ut enim ad minim veniam, quis 
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                                ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            </Col>
+                            <Col xs={2}/>
+                        </Row>
                     </Col>
                     <Col xs={4}>
                         {/*imagen a cambiar en lo posible*/}
